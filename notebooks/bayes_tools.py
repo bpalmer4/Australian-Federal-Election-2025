d759f4b358ea100c7bb76924cf5574e211b01bab
bpalmer4 <palmer.bryan@gmail.com>
2023-11-08 09:17:04 +1100

    Integration of the GP, GRW and GRWLA models

--- notebooks/bayes_tools.py
+++ notebooks/bayes_tools.py
@@ -53,10 +53,6 @@ def prepare_data_for_analysis(
     n_firms = len(poll_firm.unique())
     firm_map = {code: firm for firm, code in zip(df.Brand, poll_firm)}
 
-    # measurement error - published sample sizes are rarely effective sample sizes
-    assumed_sample_size = 750
-    measurement_error_sd = np.sqrt((50.0 * 50.0) / assumed_sample_size)
-
     # Information
     if verbose:
         print(
@@ -65,7 +61,6 @@ def prepare_data_for_analysis(
             f"Number of days: {n_days}\n"
             f"Number of pollsters: {n_firms}\n"
             f"Centre offset: {centre_offset}\n"
-            f"Measurement sd: {measurement_error_sd}\n"
             f"Pollster map: {firm_map}\n"
             f"Polling days:\n{poll_day.values}\n"
         )
@@ -73,6 +68,7 @@ def prepare_data_for_analysis(
 
 
 # --- Bayesian models and model components
+# - Gaussian Random Walk
 def guess_start(inputs: dict[str, Any]) -> np.float64:
     """Guess a starting point for the random walk,
     based on early poll results."""
@@ -84,22 +80,32 @@ def guess_start(inputs: dict[str, Any]) -> np.float64:
     return educated_guess
 
 
-def temporal_model(inputs: dict[str, Any], model: pm.Model) -> pt.TensorVariable:
+def temporal_model(
+    inputs: dict[str, Any], model: pm.Model, **kwargs
+) -> pt.TensorVariable:
     """The temporal (hidden daily voting intention) model component.
     Used in Gaussian Random Walk (GRW) models.
     Note: setting the innovation through a prior
     often results in a model that needs many samples to
     overcome poor traversal of the posterior."""
 
+    # check for specified parameters
+    innovation = kwargs.get("innovation", None)
+
+    # construct the temporal model
     with model:
+        if innovation is None:
+            # this does not mix well, but I haven't found anything that does
+            beta_hint = {"alpha": 3, "beta": 0.5}
+            print(f"innovation InverseGamma prior: {beta_hint}")
+            innovation = pm.InverseGamma("innovation", **beta_hint)
+
         init_guess_sigma = 5.0  # SD for initial guess
         start_dist = pm.Normal.dist(mu=guess_start(inputs), sigma=init_guess_sigma)
-        innovation = 0.175  # to set by hand, comment out the next line
-        # innovation = pm.TruncatedNormal("innovation", lower=0.0001, upper=0.5,
-        #                                 mu=innovation, sigma=0.1)
+
         voting_intention = pm.GaussianRandomWalk(
             "voting_intention",
-            mu=0,  # no drift in model
+            mu=0,  # no drift in this model
             sigma=innovation,
             init_dist=start_dist,
             steps=inputs["n_days"],
@@ -125,21 +131,40 @@ def house_effects_model(inputs: dict[str, Any], model: pm.Model) -> pt.TensorVar
     return house_effects
 
 
+def _get_sigma_likelihood(model, kwargs: dict) -> float | pt.TensorType:
+    """Sigma for the likelihood if not already specified."""
+
+    sigma_likelihood = kwargs.get("sigma_likelihood", None)
+    with model:
+        if sigma_likelihood is None:
+            sigma_likelihood_hint = {"sigma": 5}
+            print(f"sigma_likelihood HalfNormal prior: {sigma_likelihood_hint}")
+            sigma_likelihood = pm.HalfNormal(
+                "sigma_likelihood", **sigma_likelihood_hint
+            )
+    return sigma_likelihood
+
+
 def observational_model(
     inputs: dict[str, Any],
     model: pm.Model,
     voting_intention: pt.TensorVariable,
     house_effects: pt.TensorVariable,
+    **kwargs,
 ) -> None:
     """Observational model (likelihood) component.
     Used with GRW models."""
 
+    # check for specified parameters
+
     with model:
+        sigma_likelihood = _get_sigma_likelihood(model, kwargs)
+
         pm.Normal(
             "polling_observations",
             mu=voting_intention[inputs["poll_day"].values]
             + house_effects[inputs["poll_firm"].values],
-            sigma=inputs["measurement_error_sd"],
+            sigma=sigma_likelihood,
             observed=inputs["zero_centered_y"],
         )
         if inputs["left_anchor"] is not None:
@@ -162,7 +187,7 @@ def observational_model(
             )
 
 
-def grw_model(inputs: dict[str, Any]) -> pm.Model:
+def grw_model(inputs: dict[str, Any], **kwargs) -> pm.Model:
     """PyMC model for pooling/aggregating voter opinion polls,
     using a Gaussian random walk (GRW). Model assumes poll data
     (in percentage points) has been zero-centered (by
@@ -171,20 +196,20 @@ def grw_model(inputs: dict[str, Any]) -> pm.Model:
     effects sum to zero."""
 
     model = pm.Model()
-    voting_intention = temporal_model(inputs, model)
+    voting_intention = temporal_model(inputs, model, **kwargs)
     house_effects = house_effects_model(inputs, model)
-    observational_model(inputs, model, voting_intention, house_effects)
+    observational_model(inputs, model, voting_intention, house_effects, **kwargs)
     return model
 
 
-def grw_la_model(inputs: dict[str, Any]) -> pm.Model:
+def grw_la_model(inputs: dict[str, Any], **kwargs) -> pm.Model:
     """A more nuanced left_anchor model which captures the
     systemic polling error in the posterior. But otherwise,
     much the same as the left anchor version of grw_model()."""
 
     assert inputs["left_anchor"] is not None
     model = pm.Model()
-    voting_intention = temporal_model(inputs, model)
+    voting_intention = temporal_model(inputs, model, **kwargs)
 
     with model:
         poll_error_sigma = 2  # assume smaller systemic poll error possible
@@ -202,103 +227,142 @@ def grw_la_model(inputs: dict[str, Any]) -> pm.Model:
             var=zero_sum_house_effects + systemic_poll_error,
         )
 
-    observational_model(inputs, model, voting_intention, house_effects)
+    observational_model(inputs, model, voting_intention, house_effects, **kwargs)
     return model
 
 
+# - Gaussian Process model ...
 def gp_prior(
-    inputs: dict[str, Any], 
+    inputs: dict[str, Any],
     model: pm.Model,
-    length_scale: Optional[float] = None,
-    eta: Optional[float] = None,
+    **kwargs,
 ) -> pt.TensorVariable:
     """Construct the Gaussian Process (GP) latent variable model prior.
     The prior reflects voting intention on specific polling days.
-    
+
     Note: Reasonably smooth looking plots only emerge with a lenjth_scale
     greater than (say) 15. Divergences occur when eta resolves as being
-    close to zero, (which is obvious when you think about it, but also 
+    close to zero, (which is obvious when you think about it, but also
     harder to avoid with series that are fairly flat). To address
     these sampling issues, we give the gamma distribution a higher alpha,
     as the mean of the gamma distribution is a/b. And we truncate eta to
     well avoid zero (noting eta is squared before being multiplied by the
     covariance matrix).
-    
+
     Also note: for quick test runs, length_scale and eta can be fixed
     to (say) 20 and 1.2 respectively. With both specified, the model runs
-    in around 1.4 seconds. With one or both unspecified, it takes about 
-    7 minutes per run."""
+    in around 1.4 seconds. With one or both unspecified, it takes about
+    7-8 minutes per run."""
 
+    # check for specified parameters
+    length_scale = kwargs.get("length_scale", None)
+    eta = kwargs.get("eta", None)
+
+    # construct the gaussian prior
     with model:
         if length_scale is None:
-            gamma_hint = {"alpha": 20, "beta": 1}  # ideally a=20, b=1
+            gamma_hint = {"alpha": 20, "beta": 1}
+            print(f"length_scale Gamma prior: {gamma_hint}")
             length_scale = pm.Gamma("length_scale", **gamma_hint)
         if eta is None:
-            eta = pm.TruncatedNormal("eta", mu=1, sigma=5, lower=0.5, upper=20)
+            halfnormal_hint = {"sigma": 5}
+            print(f"eta HalfNormal prior: {halfnormal_hint}")
+            eta = pm.HalfNormal("eta", **halfnormal_hint)
         cov = eta**2 * pm.gp.cov.ExpQuad(1, length_scale)
         gp = pm.gp.Latent(cov_func=cov)
-        gauss_prior = gp.prior("gauss_prior", X=inputs["poll_day_c_"])
-    return gauss_prior
+        voting_intention = gp.prior("voting_intention", X=inputs["poll_day_c_"])
+    return voting_intention
 
 
 def gp_likelihood(
     inputs: dict[str, Any],
     model: pm.Model,
-    gauss_prior: pt.TensorVariable,
+    voting_intention: pt.TensorVariable,
     house_effects: pt.TensorVariable,
+    **kwargs,
 ) -> None:
-    """Observational model (likelihood) - Gaussian Process model."""
+    """Observational model (likelihood) - Gaussian Process model.
 
+    Two possible likelihood distributions specified by
+    "approach" in kwargs: 'Normal' or 'StudentT'.
+
+    Note: nu and sigma can be fixed floats rather than
+    distributions by specification in kwargs. Possible
+    values include: nu=20, sigma=1.2"""
+
+    # check for specified parameters
+    likelihood = kwargs.get("likelihood", "Normal")
+    nu = kwargs.get("nu", None)
+
+    # construct the likelihood
     with model:
-        # Normal observational model
-        pm.Normal(
-            "observed_polls",
-            mu=gauss_prior + house_effects[inputs["poll_firm"]],
-            sigma=inputs["measurement_error_sd"],
-            observed=inputs["zero_centered_y"],
-        )
+        sigma_likelihood = _get_sigma_likelihood(model, kwargs)
+        common_args = {
+            "name": "observed_polls",
+            "mu": voting_intention + house_effects[inputs["poll_firm"]],
+            "sigma": sigma_likelihood,
+            "observed": inputs["zero_centered_y"],
+        }
+
+        match likelihood:
+            case "Normal":
+                pm.Normal(**common_args)
+
+            case "StudentT":
+                if nu is None:
+                    nu_hint = {"alpha": 2, "beta": 0.1}
+                    print(f"nu Gamma prior: {nu_hint}")
+                    nu = pm.Gamma("nu", **nu_hint) + 1
+
+                pm.StudentT(
+                    **common_args,
+                    nu=nu,
+                )
 
 
 def gp_model(inputs: dict[str, Any], **kwargs) -> pm.Model:
     """PyMC model for pooling/aggregating voter opinion polls,
-    using a Gaussian Process (GP). Note: kwargs allows one to pass
-    length_scale and eta to gp_prior()."""
+    using a Gaussian Process (GP). Note: **kwargs allows one to
+    pass length_scale and eta to gp_prior() and/or approach,
+    nu and sigma to gp_likelihood()."""
 
     model = pm.Model()
-    gauss_prior = gp_prior(inputs, model, **kwargs)
+    voting_intention = gp_prior(inputs, model, **kwargs)
     house_effects = house_effects_model(inputs, model)
-    gp_likelihood(inputs, model, gauss_prior, house_effects)
+    gp_likelihood(inputs, model, voting_intention, house_effects, **kwargs)
     return model
 
 
 # --- Condition the model on the data
-def report_glitches(idata: az.InferenceData) -> None:
+def report_glitches(idata: az.InferenceData) -> str:
     """Display some quick summary diagnostics."""
 
-    def check(name: str, mask: pd.Series, summary: pd.DataFrame) -> None:
-        if mask.any():
-            table = summary[mask]
-            print(f"\n--- CHECK --- {name} for {table.index.values}")
-            display(table)
-
+    glitches = []
     summary = az.summary(idata)
 
     max_r_hat = 1.01
-    print(f"Maximum r_hat: {summary.r_hat.max()}")
-    check("r_hat", summary.r_hat > max_r_hat, summary)
+    statistic = summary.r_hat.max()
+    print(text := f"max_r_hat: {statistic}")
+    if statistic > max_r_hat:
+        glitches.append(text)
 
     min_ess = 500
-    print(f"Minimum ess: {summary[['ess_tail', 'ess_bulk']].min().min()}")
-    mask = (summary.ess_tail <= min_ess) | (summary.ess_bulk <= min_ess)
-    check("ess", mask, summary)
+    statistic = summary[["ess_tail", "ess_bulk"]].min().min()
+    print(text := f"min_ess: {statistic}")
+    if statistic < min_ess:
+        glitches.append(text)
 
     try:
-        if (diverging_count := np.sum(idata.sample_stats.diverging)) > 0:
-            print(f"\n--- CHECK --- There were {int(diverging_count)} divergences")
+        diverging_count = int(np.sum(idata.sample_stats.diverging))
     except (ValueError, AttributeError):  # No sample_stats, or no `.diverging`
         diverging_count = 0
-    if not diverging_count:
-        print("No divergences detected in the Inference Data")
+    print(text := f"{diverging_count} divergences")
+    if diverging_count:
+        glitches.append(text)
+
+    return (
+        "" if not glitches else f'Issues with Bayesian sampling: {", ".join(glitches)}'
+    )
 
 
 def draw_samples(model: pm.Model, n_cores: int = 10, **kwargs) -> az.InferenceData:
@@ -313,11 +377,28 @@ def draw_samples(model: pm.Model, n_cores: int = 10, **kwargs) -> az.InferenceDa
             **kwargs,
         )
         az.plot_trace(idata)
-    report_glitches(idata)
-    return idata
+    glitches = report_glitches(idata)
+    return (idata, glitches)
 
 
 #  --- Plotting support ...
+def generate_model_map(
+    model: pm.Model,
+    filemame_stem: str,
+    model_dir: str = "../model-images/",
+    display_images: bool = False,
+) -> None:
+    """Generate a map image for the model."""
+
+    gv = pm.model_to_graphviz(model)
+    gv.render(
+        format="png",
+        filename=(f"{model_dir}" f"model-graphviz-{filemame_stem.replace('/', '')}"),
+    )
+    if display_images:
+        display(gv)
+
+
 def plot_univariate(
     idata: az.InferenceData,
     var_names: str | Iterable[str],
@@ -338,7 +419,7 @@ def plot_univariate(
         defaults = {  # default arguments for finalise_plot()
             "xlabel": None,
             "ylabel": None,
-            "title": f"{title_stem}{variable}",
+            "title": f"{title_stem}: {variable}",
             "show": False,
             **plotting.footers,
         }
@@ -346,7 +427,7 @@ def plot_univariate(
         plotting.finalise_plot(axes, **defaults, **kwargs_copy)
 
 
-PERCENTS = [1, 10, 25, 40, 49]
+PERCENTS = [2.5, 12.5, 25, 37.5, 47.5]
 _COLORS = [(p - min(PERCENTS)) / (max(PERCENTS) - min(PERCENTS)) for p in PERCENTS]
 MIN_COLOR = 0.25
 COLOR_FRACS = [c * (1.0 - MIN_COLOR) + MIN_COLOR for c in _COLORS]
@@ -364,11 +445,11 @@ def _get_var(var_name: str, idata: az.InferenceData) -> pd.DataFrame:
     )
 
 
-def plot_voting(inputs, idata, var_name, palette, title_stem, **kwargs) -> pd.Series:
+def plot_voting(inputs, idata, palette, **kwargs) -> pd.Series:
     """Plot voting intention from both GRW and GP models."""
 
     # get the relevant data as a DataFrame
-    df = _get_var(var_name, idata) - inputs["centre_offset"]
+    df = _get_var("voting_intention", idata) - inputs["centre_offset"]
     df.index = (
         [x.to_timestamp() for x in inputs["poll_date"]]
         if len(df) == inputs["n_polls"]
@@ -382,10 +463,10 @@ def plot_voting(inputs, idata, var_name, palette, title_stem, **kwargs) -> pd.Se
     cmap = plt.get_cmap(palette)
     x = df.index
     # plot a sample of the samples
-    for y in df.sample(n=100, axis=1):
-        ax.plot(
-            df.index, df[y], color=cmap(0.9), lw=0.25, alpha=0.5, label=None, zorder=0
-        )
+    # for y in df.sample(n=100, axis=1):
+    #    ax.plot(
+    #        df.index, df[y], color=cmap(0.9), lw=0.25, alpha=0.5, label=None, zorder=0
+    #    )
     # plot quantiles
     for i, p in enumerate(PERCENTS):
         quants = p, 100 - p
@@ -415,16 +496,19 @@ def plot_voting(inputs, idata, var_name, palette, title_stem, **kwargs) -> pd.Se
     lo, hi = ax.get_ylim()
     halfway = (hi - lo) / 2 + lo
     space = "lower" if middle.iloc[0] > halfway else "upper"
-    plotting.finalise_plot(
-        ax,
-        title=f"Voting intention: {title_stem}",
-        ylabel="Per cent",
-        y50=True,
-        legend=plotting.LEGEND_SET
+    defaults = {  # default arguments for finalise_plot()
+        "ylabel": "Per cent",
+        "xlabel": None,
+        "show": False,
+        "y50": True,
+        "legend": plotting.LEGEND_SET
         | {"fontsize": "xx-small", "loc": f"{space} left", "ncols": 2},
-        concise_dates=True,
-        **kwargs,
-    )
+        "concise_dates": True,
+        **plotting.footers,
+    }
+    kwargs_copy, defaults = plotting.generate_defaults(kwargs, defaults)
+    plotting.finalise_plot(ax, **defaults, **kwargs_copy)
+
     return middle
 
 
@@ -434,7 +518,7 @@ def plot_house_effects(
     palette: str,
     **kwargs,
 ) -> None:
-    """Plot the House effects."""
+    """Plot the House effects for both GRW and GP models."""
 
     # get the data as a DataFrame
     df = _get_var("house_effects", idata).rename(index=inputs["firm_map"])
@@ -479,3 +563,57 @@ def plot_house_effects(
     }
     kwargs_copy, defaults = plotting.generate_defaults(kwargs, defaults)
     plotting.finalise_plot(ax, **defaults, **kwargs_copy)
+
+
+def plot_std_set(
+    inputs: dict[str, Any],
+    idata: az.InferenceData,
+    title_stem: str,
+    glitches: str,
+    show: bool,
+) -> pd.Series:  # returns the median sample
+    """Produce the standard set of plots for a Bayesian
+    Analysis. Return the median sample."""
+
+    core_plot_args = {
+        "show": show,
+        "rheader": None if not glitches else {"rheader": glitches},
+    }
+
+    # plot single variables from the models
+    plot_univariate(
+        idata,
+        var_names=(
+            "length_scale",
+            "eta",
+            "nu",
+            "sigma_likelihood",
+            "innovation",
+            "alpha",
+            "beta",
+            "systemic_poll_error",
+        ),
+        title_stem=title_stem,
+        **core_plot_args,
+    )
+
+    # plot voting intention over time
+    palette = plotting.get_gp_palette(title_stem)
+    middle = plot_voting(
+        inputs,
+        idata,
+        palette,
+        title=f"Vote share: {title_stem}",
+        **core_plot_args,
+    )
+
+    # plot house effects
+    plot_house_effects(
+        inputs,
+        idata,
+        palette,
+        title=f"House Effects: {title_stem}",
+        **core_plot_args,
+    )
+
+    return middle
